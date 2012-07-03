515ceb80a7a37db3996bbbfe3448e4586b6e7fad
120011676@qq.com <120011676@qq.com>
2012-07-03 17:10:00 +0800

    proxy http complete

--- redspider/src/main/java/org/qq120011676/redspider/core/RedSpiderCore.java
+++ redspider/src/main/java/org/qq120011676/redspider/core/RedSpiderCore.java
@@ -5,14 +5,49 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.Proxy;
+import java.net.Proxy.Type;
 import java.net.URL;
 
 public class RedSpiderCore {
 
-	public void call(String url) throws IOException {
-		HttpURLConnection connection = (HttpURLConnection) new URL(url)
-				.openConnection();
-		//connection.setRequestMethod("POST");
+	public String call(String url) throws IOException {
+		return this.call(this.getHttpURLConnection(url));
+	}
+
+	public String call(String url, String host, int port) throws IOException {
+		return this.call(this.getHttpURLConnection(url, host, port));
+	}
+
+	public int callCode(String url) throws IOException {
+		return this.callCode(url, "", 0);
+	}
+
+	public int callCode(String url, String host, int port) throws IOException {
+		return this.getHttpURLConnection(url, host, port).getResponseCode();
+	}
+
+	private HttpURLConnection getHttpURLConnection(String url)
+			throws MalformedURLException, IOException {
+		return (HttpURLConnection) new URL(url).openConnection();
+	}
+
+	private HttpURLConnection getHttpURLConnection(String url, String host,
+			int port) throws MalformedURLException, IOException {
+		HttpURLConnection connection;
+		if (host != null && !"".equals(host.trim())) {
+			Proxy proxy = new Proxy(Type.HTTP,
+					new InetSocketAddress(host, port));
+			connection = (HttpURLConnection) new URL(url).openConnection(proxy);
+		} else {
+			connection = (HttpURLConnection) new URL(url).openConnection();
+		}
+		return connection;
+	}
+
+	private String call(HttpURLConnection connection) throws IOException {
 		if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
 			InputStream input = connection.getInputStream();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
@@ -20,10 +55,12 @@ public class RedSpiderCore {
 			String line = null;
 			StringBuffer sb = new StringBuffer();
 			while ((line = reader.readLine()) != null) {
-				sb.append(line);
+				sb.append(line).append("\r");
 			}
 			reader.close();
 			connection.disconnect();
+			return sb.toString();
 		}
+		return null;
 	}
 }
--- redspider/src/test/java/org/qq120011676/redspider/core/TestRedSpiderCore.java
+++ redspider/src/test/java/org/qq120011676/redspider/core/TestRedSpiderCore.java
@@ -7,8 +7,11 @@ import org.junit.Test;
 public class TestRedSpiderCore {
 
 	@Test
-	public void test() throws IOException{
-		RedSpiderCore core = new RedSpiderCore();
-		core.call("http://www.baidu.com");
+	public void test() throws IOException {
+		// RedSpiderCore core = new RedSpiderCore();
+		// String code = core.call("http://www.baidu.com");
+		// System.out.println(code);
+		System.out.println(new RedSpiderCore().callCode(
+				"http://222.211.85.18:9004/", "113.106.48.103", 80));
 	}
 }

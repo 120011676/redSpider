260a8e74281823beb0a977385f1d7c5a004ac473
Say <120011676@qq.com>
2013-03-21 16:19:02 +0800

    update

---
+++ redspider/src/test/java/org/qq120011676/redspider/core/RedSpiderTest.java
@@ -0,0 +1,23 @@
+package org.qq120011676.redspider.core;
+
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+/**
+ * <b>@author</b> Say<br>
+ * <b>email</b> 120011676@qq.com<br>
+ */
+public class RedSpiderTest {
+
+	@Test
+	public void get() throws IOException, JSONException {
+		RedSpiderCore redSpiderCore = new RedSpiderCore();
+		String r = redSpiderCore.get("http://app.cditv.tv/list-28-1-13.html");
+		System.out.println(r);
+		System.out.println(new JSONObject(r).getJSONArray("result")
+				.getJSONObject(0).getString("title"));
+	}
+}
